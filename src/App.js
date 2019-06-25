123c2e0b5050ab6ade39e269138e1801707af4cc
Ann Cascarano <a.cascarano@gmail.com>
2019-06-25 15:03:19 -0400

    first pass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+// import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<h1>Hello from React!</h1>
+				<Navbar icon='fab fa-github' title='GitHub Finder' />
+				<div className='container'>
+					<Users />
+				</div>
 			</div>
 		);
 	}
