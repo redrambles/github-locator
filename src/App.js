04bb2928245a4795e9dc613d02c6072e72defa41
Ann Cascarano <a.cascarano@gmail.com>
2019-06-27 14:30:56 -0400

    Add proper routing to nav and User component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
+import { async } from "q";
 
 class App extends Component {
 	state = {
 		users: [],
+		user: {},
 		loading: false,
 		alert: null
 	};
@@ -37,6 +40,16 @@ class App extends Component {
 		this.setState({ users: res.data.items, loading: false });
 	};
 
+	getUser = async username => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${
+				process.env.REACT_APP_GITHUB_CLIENT_ID
+			}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({ user: res.data, loading: false });
+	};
+
 	clearUsers = () => {
 		this.setState({ users: [] });
 	};
@@ -49,7 +62,7 @@ class App extends Component {
 	};
 
 	render() {
-		const { users, loading } = this.state;
+		const { users, user, loading } = this.state;
 		return (
 			<Router>
 				<div className='App'>
@@ -73,6 +86,18 @@ class App extends Component {
 								)}
 							/>
 							<Route exact path='/about' component={About} />
+							<Route
+								exact
+								path='/user/:login'
+								render={props => (
+									<User
+										{...props}
+										getUser={this.getUser}
+										user={user}
+										loading={loading}
+									/>
+								)}
+							/>
 						</Switch>
 					</div>
 				</div>
